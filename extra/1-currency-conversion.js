f263256cb47090f2a3087f473ed5635af12468a5
Denis Dudar <denis.dudar11@gmail.com>
2023-05-24 16:25:18 +0300

    Completed 2 tasks from extra folder but stuck with magic ball

--- extra/1-currency-conversion.js
+++ extra/1-currency-conversion.js
@@ -5,7 +5,9 @@
   Write a function that converts a price to USD (exchange rate is 1.4 $ to £)
 */
 
-function convertToUSD() {}
+function convertToUSD(money) {
+  return money * 1.4;
+}
 
 /*
   CURRENCY CONVERSION
@@ -15,7 +17,11 @@ function convertToUSD() {}
   They have also decided that they should add a 1% fee to all foreign transactions, which means you only convert 99% of the £ to BRL.
 */
 
-function convertToBRL() {}
+function convertToBRL(money) {
+  let result = (5.7 * 0.99 * money).toFixed(2);
+  let strToNum = Number(result)
+  return  strToNum;
+}
 
 /* ======= TESTS - DO NOT MODIFY ===== 
 There are some Tests in this file that will help you work out if your code is working.
